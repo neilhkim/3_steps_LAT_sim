31bde4056b95b3d07d859b61c5eea881a9379502
neilhkim <64168717+neilhkim@users.noreply.github.com>
2022-07-31 21:49:36 -0700

    Create simulate_a_single_cell.py

---
+++ run_test/simulate_a_single_cell.py
@@ -0,0 +1,10 @@
+# Simulate a single cell
+# run(
+#     ligand_densities = [.1], 
+#     kunbind_list = [0.042], 
+#     endtime=60, 
+#     plotTimeTraces=True, 
+#     nLATthreshold=3, 
+#     early_termination_option=True,
+#     probabilistic_activation=False,
+#     )
