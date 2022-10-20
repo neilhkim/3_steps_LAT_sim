b42577ec6fe715f7602d5f7554b9e3ce82b9608d
neilhkim <64168717+neilhkim@users.noreply.github.com>
2022-10-20 09:49:30 -0700

    Update get_response_curves.py

--- run_test/get_response_curves.py
+++ run_test/get_response_curves.py
@@ -7,15 +7,32 @@ sys.path.append('../')
 # importing
 from src.main import * 
 
-# Draw response curves
+# Simulating a response curve over ligand densities using speed enhancing heuristics (example):
 run(
-      ligand_densities = [0.01, .03, .1, .3, .7, 1], 
-      kunbind_list = [0.11], #[0.011, 0.042, 0.11], 
-      nCell=30, 
-      n_P_act_estimation=3,
-    #   nLATthreshold = 3, 
-      endtime=200, 
-      plotResponseCurve=True, 
-      probabilistic_activation=True,
-      early_termination_option=False,
+    kunbind_list = [1, .1], # The known kunbind of self ligand ~1/s and foreign ligand ~0.1/s  
+    ligand_densities = [.001, .01, .1, 1, 10, 30, 100], # unit: molecules per um2
+    lat_formation_propensity_fn_str = '0.3 * stats.gamma.pdf(t-3, a=2, scale=14)',
+    huge_nlat_earlyfinish_zeroifnot_used = 10,
+    plot_activation_titration_curve = True,
+    early_assume_100p = True,
     )
+
+# Showing all the options you can use:
+# run(
+#     kunbind_list = [10, 1, .1, .01, .001], # The known kunbind of MCC is 0.042/s and T102S is 0.11/s
+#     ligand_densities = [.001, .01, .1, 1, 10, 30, 100], # unit: molecules per um2
+#     n_experiment_per_ligdensity = [3,3,3,3,2,2,2],
+#     lat_formation_propensity_fn_str = '0.3 * stats.gamma.pdf(t-3, a=2, scale=14)',
+#     plot_lat_propensity = True,
+#     n_cell = 10, 
+#     endtime = 120, 
+#     n_timepoints = 100,
+#     ec50_cellactivating_latnumber = 3,
+#     huge_nlat_earlyfinish_zeroifnot_used = 10,
+#     plot_time_traces = True,
+#     plot_activation_titration_curve = True,
+#     display_progressbar = True, # works on online notebooks
+#     outfname_ifnotnull_write = '/content/drive/My Drive/Colab/1s_const',
+#     early_assume_100p = True,
+#     plat_threshold_zeroifnot_used = 0,
+#     )
